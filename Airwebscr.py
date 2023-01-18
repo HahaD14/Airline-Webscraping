a48901a42f2d5dae57b0bfa35c67c9b8acc30f55
HahaD14 <100484664+HahaD14@users.noreply.github.com>
2023-01-18 09:26:20 +0000

    Update Airwebscr.py

--- Airwebscr.py
+++ Airwebscr.py
@@ -14,18 +14,19 @@ soup = BeautifulSoup(response.content, 'html.parser')
 # find all elements with the class 'ticket-price'
 ticket_prices = soup.find_all(class_='ticket-price')
 
-# create a new Excel workbook
-workbook = openpyxl.Workbook()
+# open existing workbook
+workbook = openpyxl.load_workbook("prices.xlsx")
 
-# create a new sheet
-sheet = workbook.active
+# select the sheet to update
+sheet = workbook["Sheet1"]
 
-# add a header row
-sheet.append(["Ticket Price"])
+# get the current number of rows
+current_row = sheet.max_row
 
 # add the ticket prices to the sheet
 for price in ticket_prices:
-    sheet.append([price.text])
+    sheet.cell(row=current_row + 1, column=1).value = price.text
+    current_row += 1
 
 # save the workbook
 workbook.save("prices.xlsx")
